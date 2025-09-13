22f5374e780bb272d65f723a07d07adca4c7f4aa
rachakrit17 <rachakrit17@gmail.com>
2025-09-13 13:49:36 +0700

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,9 +1,21 @@
-// === Put your Firebase config here ===
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDMIBSNT1FkK8XYQyQFldw3txnh1DbVspM",
+  authDomain: "the-world-of-magic-wiki-thai.firebaseapp.com",
+  projectId: "the-world-of-magic-wiki-thai",
+  storageBucket: "the-world-of-magic-wiki-thai.firebasestorage.app",
+  messagingSenderId: "1051629461515",
+  appId: "1:1051629461515:web:7063ffb41838538781e70e",
+  measurementId: "G-E7G0R3QY0C"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
